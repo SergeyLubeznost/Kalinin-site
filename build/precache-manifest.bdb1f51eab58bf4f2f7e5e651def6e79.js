7a6d5dec806101865af91d872bcd79cb40043a6b
SergeyLubeznost <Kutsalov-dg-hub@mail.ru>
2024-10-12 18:44:25 +0300

    add build

---
+++ build/precache-manifest.bdb1f51eab58bf4f2f7e5e651def6e79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358357e961a99b2df03725601869fd6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0674d132fd63650396a1",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e6ed8245aedab0b504eb",
+    "url": "/static/js/2.df1e0f0a.chunk.js"
+  },
+  {
+    "revision": "09dfcff8991931712817539b48cc9b6c",
+    "url": "/static/js/2.df1e0f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c4ae90cbb18b9b1e17",
+    "url": "/static/js/3.41871827.chunk.js"
+  },
+  {
+    "revision": "0674d132fd63650396a1",
+    "url": "/static/js/main.daa7ec10.chunk.js"
+  },
+  {
+    "revision": "b0977c6a07c4e7650040",
+    "url": "/static/js/runtime-main.aeb01d97.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
