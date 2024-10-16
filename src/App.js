70c5e902087c7b21e622f2fd1643136ae9da8723
SergeyLubeznost <Kutsalov-dg-hub@mail.ru>
2024-10-16 15:44:57 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/header/header"
+import Main from "./pages/main/main"
 // import {
 //   Link,
 // } from "react-router-dom";
@@ -9,6 +10,7 @@ function Root() {
   return (
    <>
     <Header/>
+    <Main/>
    </>
   )
 }
